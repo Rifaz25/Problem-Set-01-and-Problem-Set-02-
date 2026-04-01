0b0a8e56d658cff5b491eb7b8a880245d4b4d56f
Rifaz25 <rifazulk@gmail.com>
2026-04-01 22:02:46 +0600

    Update code.py

--- code.py
+++ code.py
@@ -37,7 +37,7 @@ from sklearn.linear_model import LogisticRegression
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test) 
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
